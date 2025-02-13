ccf2bb660be8d51841e7304a73a08859f6ff2be7
fakhryhizballah <falehry88@gmail.com>
2025-02-13 12:27:32 +0700

    hapus cansel

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const webviews = require('../controllers/webviews');
 router.get('/ruangan', webviews.fomrReservasi);
-router.get('/cancel/:id', webviews.cancel);
+// router.get('/cancel/:id', webviews.cancel);
 router.post('/reservasi', webviews.postReservasi);
 router.get('/reservasi', webviews.getReservasi);
 // router.get('/short', links.short);
